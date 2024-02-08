3b1db3917358f270705958efee55ed6a6801ad1d
LCAllen <11218466+LCAllen@users.noreply.github.com>
2024-02-08 10:48:15 -0800

    Create TIC.cs

---
+++ templates/TIC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BORST.templates
+{
+    internal class TIC
+    {
+        List<Weapon> weapons = new List<Weapon>();
+        string TICDamage = "0";
+        string TICHeat = "0";
+
+        public TIC(List<Weapon> weapons) 
+        {
+            
+        }
+
+        public void calcTicDamage()
+        {
+
+        }
+    }
+}
